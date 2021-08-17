dea1ead46040ad2b3d667b7d77f35dc4e42e4683
Денис <piligrim021@gmail.com>
2021-08-17 15:26:17 +0300

    entrypoint

--- Docker/game_ms/entrypoint.sh
+++ Docker/game_ms/entrypoint.sh
@@ -1,3 +1,8 @@
 #! /bin/bash
 
+# if [ $DEBUG ]
+#     then
+#         python -m pytest tests/tests.py -q
+# fi
+
 gunicorn main:init_app -b 0.0.0.0:8001 --worker-class aiohttp.GunicornWebWorker --reload --access-logfile ${LOG_FILENAME} --log-file ${LOG_FILENAME} --log-level ${LOG_LEVEL}
