8e333f791e26f47b780c5eea90c8122448a74293
Денис <piligrim021@gmail.com>
2021-08-17 17:48:45 +0300

    RoomAdmin

--- Docker/bunker_proj/web/admin.py
+++ Docker/bunker_proj/web/admin.py
@@ -3,7 +3,29 @@ from django.contrib import admin
 from .models import *
 
 
+class RoomUserInLine(admin.TabularInline):
+    model = RoomUser
+    fields = ['username', 'player_number', 'game_sess_id']
+    extra = 0
+    can_delete = False
+
+    def has_add_permission(self, request, obj):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    
+    inlines = [RoomUserInLine]
+    list_filter = ['state']
+    ordering = ['id']
+    readonly_fields = ['created', 'updated']
+    list_display = ['id', 'initiator', 'state', 'lap', 'turn', 'created']
+
+
 admin.site.register(Player)
-admin.site.register(Room)
 admin.site.register(RoomUser)
 admin.site.register(RoomVote)
